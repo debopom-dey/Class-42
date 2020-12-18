1fc9dbf83d4b5a9820cde8374591ca42118faaca
Pruthvi31 <70841409+Pruthvi31@users.noreply.github.com>
2020-12-18 20:11:08 +0530

    Update Umbrella.js

--- Umbrella.js
+++ Umbrella.js
@@ -1,26 +1,16 @@
 class Umbrella{
-    constructor(x,y,width,height){
-    var options={
-        restituition:0.4,
-        isStatic:true,
+    constructor(x,y){
+        var options={
+            isStatic:true
+        }
+        this.image=loadImage("walking_1.png")
+        this.umbrella=Bodies.circle(x,y,50,options);
+        World.add(world,this.umbrella)
     }
-
-    this.width=width
-    this.height=height;
-    this.image=loadImage("walking_1.png")
-    this.body=Bodies.rectangle(x,y,width,height,options);
-   
-    World.add(world,this.body)
-    }
+    
     display(){
-    var pos= this.body.position;
-    push();
-    translate(pos.x,pos.y)
-
-    imageMode(CENTER);
-    image(this.image,0,0,this.width,this.height)
-    pop();
-    
-    
-    }
+        var pos= this.umbrella.position;
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y+70,300,300) 
+        }
     }
